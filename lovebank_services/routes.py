ae15a61c5903b35097aef9ee5e37551924366aca
Carlos Portillo <c.portillojr96@gmail.com>
2020-06-01 13:39:33 -0400

    Add endpoints to create/get/update/delete tasks

--- lovebank_services/routes.py
+++ lovebank_services/routes.py
@@ -1,7 +1,97 @@
-from flask import request
+from flask import jsonify, abort, request, make_response, url_for
 from lovebank_services import app    # import app from __init__.py
 from lovebank_services.models import User, Task
+
+tasks = [
+    {
+        'id': 1,
+        'title': 'Do the dishes',
+        'description': 'rinse and dry all the dishes in the kitchen sink',
+        'cost': 15,
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': 'Do the laundry',
+        'description': 'Wash and dry all the dirty clothes',
+        'cost': 10,
+        'done': False
+    }
+]
 
 @app.route("/", methods=['GET'])
 def hello():
     return "Hello, World!"
+
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'],
+                                      _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': make_public_task(task[0])})
+
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    if not request.json or 'title' not in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1 if len(tasks) > 0 else 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'cost': request.json['cost'],
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': make_public_task(task)}), 201
+
+
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and \
+            not isinstance(request.json['title'], six.string_types):
+        abort(400)
+    if 'description' in request.json and \
+            not isinstance(request.json['description'], six.string_types):
+        abort(400)
+    if 'cost' in request.json and not isinstance(request.json['cost'], int):
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description',
+                                              task[0]['description'])
+    task[0]['cost'] = request.json.get('cost', task[0]['cost'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': make_public_task(task[0])})
+
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
