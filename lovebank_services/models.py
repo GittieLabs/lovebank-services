eaa96655525a7fe00fa743485047c7af81d8a61b
Alex Chandy <alexchandy13@gmail.com>
2020-06-02 15:03:07 -0400

    Update models.py
    Added "balance" and "pending tasks" columns

--- lovebank_services/models.py
+++ lovebank_services/models.py
@@ -6,6 +6,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    balance = db.Column(db.Integer)
+    pending_tasks = db.relationship('Task', backref='user_to')
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
 
