7488e5a45fc5715e63404256546c2311fc302063
Carlos Portillo <c.portillojr96@gmail.com>
2020-06-05 03:40:29 -0400

    Add task-user relationships and serialization functions to models

--- lovebank_services/models.py
+++ lovebank_services/models.py
@@ -3,40 +3,51 @@ from datetime import datetime
 
 # User Model
 class User(db.Model):
-    ''' Define User model '''
     id = db.Column(db.Integer, primary_key=True)
+    partner_id = db.Column(db.Integer)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    tasks = db.relationship('Task', backref='user', lazy=True)
+    tasks_created = db.relationship('Task', backref='creator', lazy=True, foreign_keys='Task.creator_id')
+    tasks_received = db.relationship('Task', backref='receiver', lazy=True, foreign_keys='Task.receiver_id')
+
+    def serialize(self):
+        ''' return data as dictionary '''
+        return {
+            "id"        :   self.id,
+            "partner_id":   self.partner_id,
+            "username"  :   self.username,
+            "email"     :   self.email,
+            "tasks_received": list(task.serialize() for task in self.tasks_received)
+        }
+
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
+        return f"User('username: {self.username}', email: '{self.email}')"
 
 
 # Task Model
 class Task(db.Model):
-    ''' Define Task model '''
-    def __init__(self, id, title, description, cost, done, creator, owner, creationTime, deadline):
-        self.id = id
-        self.title = title
-        self.description = description
-        self.cost = cost
-        self.done = done
-        self.creator = creator
-        self.owner = owner
-        self.creationTime = creationTime
-        self.deadline = deadline
-
     id = db.Column(db.Integer, primary_key=True)
-    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    title = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.String(100), nullable=False)
-    cost = db.Column(db.Integer)
-    done = db.Column(db.Boolean)
-    creator = db.Column(db.String(80))
-    owner = db.Column(db.String(80))
     creationTime = db.Column(db.DateTime, default=datetime.utcnow)
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+    cost = db.Column(db.Integer, nullable=False)
     deadline = db.Column(db.DateTime)
+    done = db.Column(db.Boolean, default=False)
+
+    def serialize(self):
+        ''' return data as dictionary '''
+        return {
+            "id"          :   self.id,
+            "creationTime":   self.creationTime,
+            "creator_id"  :   self.creator_id,
+            "receiver_id" :   self.receiver_id,
+            "title"       :   self.title,
+            "description" :   self.description,
+            "cost"        :   self.cost,
+            "done"        :   self.done
+        }
 
     def __repr__(self):
         return 'Task(id: %d, title: %s, description: %s, cost: %d)' % (self.id, self.title, self.description, self.cost)
-
