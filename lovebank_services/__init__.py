1d58f7c3b52c4a3a8aa0abdb13a660b6de56d0a8
VvivienneZhu <vivienne.zhujy@gmail.com>
2020-06-07 22:19:26 -0700

    Add clear_table and prepopulate method

--- lovebank_services/__init__.py
+++ lovebank_services/__init__.py
@@ -16,6 +16,14 @@ from lovebank_services.models import Task, User
 
 db.create_all()
 from lovebank_services import routes # to avoid circular import issues, leave this import below initializatoin of db
+from lovebank_services.fake_data import *
+
+# Pre-populate database with fake data and clear tables
+clear_table(Task)
+clear_table(User)
+populate_user_table(10, True)  # If you intend to creat linked users, provide an even row number.
+populate_task_table(10)
+
 
 ''' To connect to Firebase project - uncomment below and point to service key path '''
 # cred = credentials.Certificate("firebase-key.json") # path to downloaded firebase service key
