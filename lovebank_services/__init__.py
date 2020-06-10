b409348f8656aa7ca9c8fae1435c877097a3358a
Carlos Portillo <c.portillojr96@gmail.com>
2020-06-10 07:49:41 -0400

    Minor change

--- lovebank_services/__init__.py
+++ lovebank_services/__init__.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-
 from flask_migrate import Migrate
 from firebase_admin import credentials
 import firebase_admin
@@ -19,18 +18,9 @@ print(os.environ['APP_SETTINGS'])
 
 # Create a database instance - Note: test.db will by ignored by .gitignore. Update file if necessary
 db = SQLAlchemy(app)
-from lovebank_services.models import Task, User
-
+from lovebank_services.models import Task, Person
 db.create_all()
 from lovebank_services import routes # to avoid circular import issues, leave this import below initializatoin of db
-
-from lovebank_services.fake_data import *
-
-# Pre-populate database with fake data and clear tables
-clear_table(Task)
-clear_table(User)
-# populate_user_table(10, True)  # If you intend to creat linked users, provide an even row number.
-# populate_task_table(10)
 
 ''' To connect to Firebase project - uncomment below and point to service key path '''
 # cred = credentials.Certificate("firebase-key.json") # path to downloaded firebase service key
