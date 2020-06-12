96cf6954c9b73a4ee5269d4341889b30099bf2e4
Carlos Portillo <c.portillojr96@gmail.com>
2020-06-12 17:12:29 -0400

    Update init file

--- lovebank_services/__init__.py
+++ lovebank_services/__init__.py
@@ -39,12 +39,12 @@ from lovebank_services import routes # to avoid circular import issues, leave th
 
 
 """ Connect to Firebase Project Using KMS - Uncomment below """
-secret_name = "lovebank-secret"
-response = get_secret(secret_name)
-secret_dict = json.loads(response)
-try:
-    cred = credentials.Certificate(json.loads(secret_dict['FIREBASE_SERVICE_KEY'])) # path to downloaded firebase service key
-    firebase_admin.initialize_app(cred)
-    print("Firebase Connection Successful")
-except:
-    print("Error: Firebase Connection Unsuccessful")
+# secret_name = "lovebank-secret"
+# response = get_secret(secret_name)
+# secret_dict = json.loads(response)
+# try:
+#     cred = credentials.Certificate(json.loads(secret_dict['FIREBASE_SERVICE_KEY'])) # path to downloaded firebase service key
+#     firebase_admin.initialize_app(cred)
+#     print("Firebase Connection Successful")
+# except:
+#     print("Error: Firebase Connection Unsuccessful")
