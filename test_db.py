d6b97e3c9a890f408f389948ebab7327bb523b9a
LarryPenn <49259555+LarryPenn@users.noreply.github.com>
2020-06-22 16:25:58 -0500

    updated test DB and fixed bugs

--- test_db.py
+++ test_db.py
@@ -1,4 +1,6 @@
 import unittest
+from uuid import UUID
+
 from lovebank_services import db
 from lovebank_services.models import User, Task
 from datetime import datetime
@@ -12,14 +14,14 @@ class TestDB(unittest.TestCase):
     # setUp runs before every test
     def setUp(self):
         # Create 2 user records and add them to db
-        self.user_1 = User(username='Ann', id="1cf96ab3-f693-4909-9d32-067438bc9636", partner_id=2, email='ann@test.com')
-        self.user_2 = User(username='Bob', id="49dcc18d-0e6b-43a7-82c3-c8396fc77ee5", partner_id=1, email='bob@test.com')
+        self.user_1 = User(username='Ann', firebase_uid="firebase1",  email='ann@test.com')
+        self.user_2 = User(username='Bob', firebase_uid="firebase2",  email='bob@test.com')
         db.session.add(self.user_1)
         db.session.add(self.user_2)
         db.session.commit()
         # Create 2 task records and add them to db
-        self.task_1 = Task(creator_id="1cf96ab3-f693-4909-9d32-067438bc9636", receiver_id="49dcc18d-0e6b-43a7-82c3-c8396fc77ee5", title='title_1', description='description_1', cost=10, deadline=datetime.fromtimestamp(1596477600))
-        self.task_2 = Task(creator_id="49dcc18d-0e6b-43a7-82c3-c8396fc77ee5", receiver_id="1cf96ab3-f693-4909-9d32-067438bc9636", title='title_2', description='description_2', cost=25, deadline=datetime.fromtimestamp(1608442620))
+        self.task_1 = Task(creator_id=self.user_1.id, receiver_id=self.user_2.id, title='title_1', description='description_1', cost=10, deadline=datetime.fromtimestamp(1596477600))
+        self.task_2 = Task(creator_id=self.user_2.id, receiver_id=self.user_1.id, title='title_2', description='description_2', cost=25, deadline=datetime.fromtimestamp(1608442620))
         db.session.add(self.task_1)
         db.session.add(self.task_2)
         db.session.commit()
@@ -27,8 +29,8 @@ class TestDB(unittest.TestCase):
     # tearDown runs after every test
     def tearDown(self):
         # Delete users and tasks
-        Task.query.filter_by(creator_id="1cf96ab3-f693-4909-9d32-067438bc9636").delete()
-        Task.query.filter_by(creator_id="49dcc18d-0e6b-43a7-82c3-c8396fc77ee5").delete()
+        Task.query.filter_by(title='title_1').delete()
+        Task.query.filter_by(title='title_2').delete()
         User.query.filter_by(username = 'Ann').delete()
         User.query.filter_by(username = 'Bob').delete()
 
