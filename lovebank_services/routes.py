1612ef1eeb5add3565f1bd122945fc7c4cca0bd0
Carlos Portillo <c.portillojr96@gmail.com>
2020-06-23 05:27:59 -0400

    Add route to generate invitation code to link users

--- lovebank_services/routes.py
+++ lovebank_services/routes.py
@@ -3,6 +3,7 @@ from lovebank_services import app, db
 from lovebank_services.models import User, Task
 from lovebank_services.fake_data import *
 from datetime import datetime
+from uuid import uuid4
 import os
 
 GLOBAL_ID = 0
@@ -53,7 +54,6 @@ def update_task(task_id):
             task.title = request.json['title']
         if 'cost' in request.json:
             task.cost = request.json['cost']
-
         db.session.commit()
         return jsonify(Task.query.filter_by(id=task_id).first().serialize())
     abort(404)
@@ -88,14 +88,39 @@ def create_user():
     return jsonify(user.serialize())
 
 
-@app.route('/users/link', methods=['POST'])
-# Need a JSON file specifying the requester firebase_UID and receiver firebase_UID
+@app.route('/users/code', methods=['PUT'])
+def get_code():
+    if 'requester_id' in request.json:
+        user = User.query.filter_by(id=request.json['requester_id']).first()
+        if user:
+            # create list of active invite codes - it will be used to ensure uniqueness of generated code
+            active_codes = [item[0] for item in db.session.query(User.invite_code).filter(User.invite_code != None).all()]
+            # generate new invite code
+            new_code = str(uuid4())[:8] # slice to keep length at 8
+            # if code already exist, generate new code until unique code is found
+            while new_code in active_codes:
+                new_code = str(uuid4())[:8]
+            user.invite_code = new_code
+            db.session.commit()
+            return jsonify(user.serialize())
+        return {'Error' : 'User not found'}
+    abort(400)
+
+
+@app.route('/users/link', methods=['PUT'])
+# Need a JSON object specifying the invite code and receiver id
 def link_user():
-    if not request.json:
+    if not request.json or 'invite_code' not in request.json or 'receiver_id' not in request.json:
         abort(400)
-    user1 = User.query.filter_by(firebase_uid=request.json['requester_firebase_uid']).first()
-    user2 = User.query.filter_by(firebase_uid=request.json['receiver_firebase_uid']).first()
-    if user1.partner_firebase_uid or user2.partner_firebase_uid:
+    # find requester with invite code
+    user1 = User.query.filter_by(invite_code=request.json['invite_code']).first()
+    # if requester is not found, invitation code is invalid
+    if not user1:
+        return {'Error': 'Invalid invitation code'}
+    # find receiver with id passed in JSON object
+    user2 = User.query.filter_by(id=request.json['receiver_id']).first()
+
+    if user1.partner_id or user2.partner_id:
         return {'Error': 'Invalid request'}
     else:
         user1.partner_firebase_uid = user2.firebase_uid,
@@ -108,14 +133,14 @@ def link_user():
         return {'user1 partner': user1.partner_firebase_uid}
 
 
-@app.route('/users/delink', methods=['POST'])
+@app.route('/users/delink', methods=['PUT'])
 # Need a JSON file specifying the requester firebase_UID and receiver firebase_UID
 def delink_user():
-    if not request.json:
+    if not request.json or 'requester_id' not in request.json or 'receiver_id' not in request.json:
         abort(400)
-    user1 = User.query.filter_by(firebase_uid=request.json['requester_firebase_uid']).first()
-    user2 = User.query.filter_by(firebase_uid=request.json['receiver_firebase_uid']).first()
-    if (user1.partner_firebase_uid != user2.firebase_uid) or (user2.partner_firebase_uid != user1.firebase_uid):
+    user1 = User.query.filter_by(id=request.json['requester_id']).first()
+    user2 = User.query.filter_by(id=request.json['receiver_id']).first()
+    if (user1.partner_id != user2.id) or (user2.partner_id != user1.id):
         return {'Error': 'Invalid request'}
     #the delink request is only valid when the two users are connected to each other already
     else:
