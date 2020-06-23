3134e8d6040791f3d8f7f4366f5ab8c46dc377d4
BabyCube <yifei.han-123@outlook.com>
2020-06-22 22:14:54 -0400

    Added backend individual notification functionality

--- notification_service/individual_notification.py
+++ notification_service/individual_notification.py
@@ -1,25 +1,22 @@
 import firebase_admin
-from firebase_admin import messaging, credentials
+from firebase_admin import messaging, credentials, firestore
 
 cred = credentials.Certificate("love-bank-9a624-firebase-adminsdk-r81q1-9c75f8d0dc.json") # shared within keybase
 firebase_admin.initialize_app(cred)
+firestore_db = firestore.client()
 
-def send_to_token(device_token):
+def send_to_device(device_token, title, body):
+    """this function will send notification only to one device with token specified"""
     # [START send_to_token]
     # This registration token comes from the client FCM SDKs.
-    registration_token = 'YOUR_REGISTRATION_TOKEN'
 
     # See documentation on defining a message payload.
     notification = messaging.Notification(
-        title="test notification title",
-        body="test notification body"
+        title=title,
+        body=body
     )
 
     message = messaging.Message(
-        data={
-            'score': '850',
-            'time': '2:45',
-        },
         notification=notification,
         token=device_token,
     )
@@ -31,4 +28,12 @@ def send_to_token(device_token):
     print('Successfully sent message:', response)
     # [END send_to_token]
 
-send_to_token("insert token here")
+def send_to_user(user_token, title, body):
+    """this function will take user UID in firebase and end to each device of this user known"""
+
+    users_ref = firestore_db.collection(u'user_collection').document(user_token).collection(u'device')
+    all_devices = users_ref.stream()
+
+    for device in all_devices:
+        send_to_device(device.id, title, body)
+
