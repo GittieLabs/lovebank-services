f2d7df342566a7e58b397832d2736d3e5fe2a00f
william <191marquis@gmail.com>
2020-06-30 20:01:13 -0400

    [DEV] Add tests for /users endpoints

--- test_routes.py
+++ test_routes.py
@@ -1,6 +1,10 @@
-""" pytest for flask API  
+"""
+    pytest for flask API  
     see conftest.py file for fixture config
 """
+import pytest
+import json
+from firebase_admin import auth
 
 def test_home_page(test_client):
     """
@@ -57,3 +61,76 @@ def test_tasks_post_by_id(test_client):
     response = test_client.post('/tasks/1cf96ab3-f693-4909-9d32-067438bc9636')
     assert response.status_code == 405
     assert response.content_type == 'text/html; charset=utf-8'
+
+
+
+### Below is the user endpoints tests.
+# DELETE /users (to clear all users) isn't
+# tested here due to the risk of accidentally
+# clearing all data in the wrong database.
+
+create_delete_success = False
+
+@pytest.mark.skip(reason="Utility function")
+def create_test_user(test_client, email):
+    """
+        utility function to create a test user
+    """
+
+    user = auth.create_user(email=email, password="12345678")
+    fid = user.uid
+
+    response = test_client.post('/users', data=json.dumps({'firebase_uid': fid, 
+                                                           'username': 'create delete pytest',
+                                                           'email': email}), content_type='application/json')
+    return response
+
+
+@pytest.mark.skip(reason="Utility function")
+def delete_test_user(test_client, uid):
+    """
+        utility function to delete a test user
+    """
+    response = test_client.delete('/users/'+uid)
+    return response
+
+
+
+def test_create_delete_users(test_client):
+    """
+    GIVEN a flask application
+    WHEN the /users endpoint is called with POST or DELETE (individual delete)
+    THEN check the user creates and deletes successfully
+    """
+    email = "test@pytest.pytest"
+
+    response = create_test_user(test_client, email)
+    response_data = json.loads(response.get_data(as_text=True))
+    assert(response.status_code == 200)
+
+    uid = response_data['id']
+
+    response = delete_test_user(test_client, uid)
+    response_data = json.loads(response.get_data(as_text=True))
+
+    assert(response.status_code == 200)
+    assert(response_data["result"] == True)
+
+    create_delete_success = True
+
+def test_populate_users():
+    pass
+
+def test_get_users():
+    pass
+
+def test_invite_users():
+    pass
+
+def test_link_users():
+    pass
+
+def test_unlink_users():
+    pass
+
+
