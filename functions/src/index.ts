78b0e01feda813780b74a382599bd3d2b43672d7
Carlos Portillo <c.portillojr96@gmail.com>
2020-07-14 15:14:06 -0400

    Refactor code, add error ID logging for debugging

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,45 +3,29 @@ import * as admin from 'firebase-admin'
 import {v4 as uuid4} from 'uuid'
 admin.initializeApp()
 
-// Hello World 
+// Hello world 
 export const helloWorld = functions.https.onRequest((req, res) => {
     res.status(200).send('Welcome to Lovebank!')
 })
 
-// Function to update users (generate invite code, create a user, or accept an invite code)
+// Generate invite code
 export const users = functions.https.onRequest(async (req, res) => {
-    const db = admin.firestore()
-    switch(req.method) {
-        case 'PUT':
-            // Generate code
-            if (req.body.action === 'invite' && req.body.id) {
-                const inviteCode = uuid4()
-                const userRef = db.doc(`users/${req.body.id}`)
-                try {
-                    await userRef.update({"invite-code" : `${inviteCode}`})
-                    const updated_user = (await db.doc(`users/${req.body.id}`).get()).data()
-                    res.status(200).send(updated_user)
-                }
-                catch (err) {
-                    res.status(500).send({Error: "There was an error in updating user"})
-                }
-            }
-            // Accept using invite code
-            else if (req.body.action === 'accept') {
-                res.status(200).send({test: 'accept under construction'})
-            }
-            // Invalid action field
-            else {
-                res.status(400).send({error: 'Invalid request'})
-            }
-            break
-
-        case 'POST': 
-            res.status(200).send('POST requested')
-            break
-
-        default:
-            res.status(405).send({error: "Method not allowed"})
-            break
+    if (req.method === 'PUT' && req.body.action === 'invite' && req.body.id){
+        const db = admin.firestore()
+        const inviteCode = uuid4()
+        const userRef = db.doc(`users/${req.body.id}`)
+        try {
+            await userRef.update({"invite-code" : `${inviteCode}`})
+            const updated_user = await db.doc(`users/${req.body.id}`).get()
+            res.status(200).send(updated_user.data())
+        }
+        catch(err) {
+            const errorID = uuid4()           // Create error id
+            console.log(`${errorID}: ${err}`) // Log error
+            res.status(500).send({Error: `There was an error in updating user. Error ID: ${errorID}`})
+        }
+    }
+    else {
+        res.status(400).send({Error: "Bad request"})
     }
 })
