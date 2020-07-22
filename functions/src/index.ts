e28fe9da343074938f9e70bd0f00585875b71018
Carlos Portillo <c.portillojr96@gmail.com>
2020-07-22 03:59:27 -0400

    Refactor accept function and add error checking

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -28,7 +28,7 @@ export const invite = functions.https.onRequest(async (req, res) => {
         }
         else {
             res.status(404).send({Error: 'User not found'})
-        }   
+        }
     }
     else {
         res.status(400).send({
@@ -40,30 +40,50 @@ export const invite = functions.https.onRequest(async (req, res) => {
 
 // Accept invite code
 export const accept = functions.https.onRequest(async(req, res) => {
-    if (req.method === 'PUT' && req.body.id && req.body.code) {
+    try {
+        // Check if request is valid
+        if (req.method != 'PUT' || !req.body.id || !req.body.code){
+            throw({status:400, message:'Request field may be missing or incorrect method used'})
+        }
+        // Check if invite document exists
         const inviteRef = db.collection('invites').where('invite_code', '==', req.body.code)
-        const invite_data = (await inviteRef.get()).docs[0].data()
-        if (invite_data) { // Check if invite document exists
-            try {
-                const requesterRef = db.doc(`users/${invite_data.requester_id}`)
-                const receiverRef = db.doc(`users/${req.body.id}`)
-                // Update both partnerIds
-                await receiverRef.update({'partnerId' : invite_data.requester_id})
-                await requesterRef.update({'partnerId' : req.body.id})
-                const updated_user = await db.doc(`users/${req.body.id}`).get()
-                res.status(200).send(updated_user.data())
-            }
-            catch(err) {
-                res.status(404).send({Error: 'User not found'})
-            }
+        const invite = (await inviteRef.get()).docs[0]
+        if (!invite) {
+            throw({status:400, message:'Invalid invite code'})
         }
-        else {
-            res.status(400).send({Error: 'Invalid invite code'})
+        // Check if users exist
+        const requesterID = invite.data().requester_id
+        const receiverID = req.body.id
+        const requester = await db.doc(`users/${requesterID}`).get()
+        const receiver = await db.doc(`users/${receiverID}`).get()
+        if (!requester.exists || !receiver.exists){
+            throw({status:404, message:'User not found'})
         }
+        // Check if user is trying to pair with self
+        if (requesterID === receiverID){
+            throw({status:400, message:'Cannot pair user with self'}) 
+        }
+        // Check if both users are unpaired
+        if (requester.data()?.partnerId || receiver.data()?.partnerId){
+            throw({status:400, message:'User already paired'})
+        }
+        // Update both partnerIds
+        await db.doc(`users/${receiverID}`).update({'partnerId' : requesterID})
+        await db.doc(`users/${requesterID}`).update({'partnerId' : receiverID})
+        const updated_user = await db.doc(`users/${receiverID}`).get()
+        // Delete invite documents associated with either user
+        await db.doc(`invites/${requesterID}`).delete()
+        await db.doc(`invites/${receiverID}`).delete()
+
+        res.status(200).send(updated_user.data())
     }
-    else {
-        res.status(400).send({
-            Error: 'Bad request. Request may be missing user id or invite code.'
-        })
+    catch (err) {
+        var status = 500
+        var message = err
+        if (err.status && err.message){
+            status = err.status
+            message = err.message
+        }
+        res.status(status).send({"Error": message})
     }
 })
