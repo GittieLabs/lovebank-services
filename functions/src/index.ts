c451995cc079c20b73374362311fa5639ef10db0
Carlos Portillo <c.portillojr96@gmail.com>
2020-07-21 22:04:49 -0400

    Update accept function to use 'invites' collection

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,7 @@ admin.initializeApp()
 
 const db = admin.firestore() // Reference to the firestore db
 
+
 // Hello world 
 export const helloWorld = functions.https.onRequest((req, res) => {
     res.status(200).send('Welcome to Lovebank!')
@@ -13,7 +14,7 @@ export const helloWorld = functions.https.onRequest((req, res) => {
 
 // Generate invite code
 export const invite = functions.https.onRequest(async (req, res) => {
-    if (req.method === 'PUT' && req.body.action === 'invite' && req.body.id && req.body.mobile){
+    if (req.method === 'POST' && req.body.action === 'invite' && req.body.id && req.body.mobile){
         const inviteCode = uuid4() // Create invite code
         const user = await db.doc(`users/${req.body.id}`).get()
         if (user.exists) {
@@ -30,26 +31,39 @@ export const invite = functions.https.onRequest(async (req, res) => {
         }   
     }
     else {
-        res.status(400).send({Error: 'Bad request. Request may be missing user id, action, or mobile'})
+        res.status(400).send({
+            Error: 'Bad request. Request may be missing user id, action, or mobile.'
+        })
     }
 })
 
 
 // Accept invite code
 export const accept = functions.https.onRequest(async(req, res) => {
-    const inviteRef = db.collection('invites').where('invite_code', '==', req.body.code)
-    try {
-        const receiverID = req.body.id
-        const requesterID = (await inviteRef.get()).docs[0].data().requester_id
-        const receiverRef = db.doc(`users/${receiverID}`)
-        const requesterRef = db.doc(`users/${requesterID}`)
-        // pair the users
-        await receiverRef.update({"partner_id": requesterID})
-        await requesterRef.update({"partner_id": receiverID})
-        res.send("Success")
+    if (req.method === 'PUT' && req.body.id && req.body.code) {
+        const inviteRef = db.collection('invites').where('invite_code', '==', req.body.code)
+        const invite_data = (await inviteRef.get()).docs[0].data()
+        if (invite_data) { // Check if invite document exists
+            try {
+                const requesterRef = db.doc(`users/${invite_data.requester_id}`)
+                const receiverRef = db.doc(`users/${req.body.id}`)
+                // Update both partnerIds
+                await receiverRef.update({'partnerId' : invite_data.requester_id})
+                await requesterRef.update({'partnerId' : req.body.id})
+                const updated_user = await db.doc(`users/${req.body.id}`).get()
+                res.status(200).send(updated_user.data())
+            }
+            catch(err) {
+                res.status(404).send({Error: 'User not found'})
+            }
+        }
+        else {
+            res.status(400).send({Error: 'Invalid invite code'})
+        }
     }
-    catch(err) {
-        console.log(err)
-        res.status(400).send({Error: "Invalid invitation code"})
-    }  
+    else {
+        res.status(400).send({
+            Error: 'Bad request. Request may be missing user id or invite code.'
+        })
+    }
 })
